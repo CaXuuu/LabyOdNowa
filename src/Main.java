6c81de6a99d9e3f4c9481958e5ae3ae8d529f383
maciek <maciekmalyszek@gmail.com>
2025-08-29 10:54:28 +0200

    Z5.2

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-Person person = new Person();
-Person person2 = new Person();
 List<Person> persons = new ArrayList<Person>();
-persons.add(person);
-persons.add(person2);
-persons.stream()
-        .forEach(System.out::println);
-System.out.println(persons);
+
 
 
     }
--- src/Person.java
+++ src/Person.java
@@ -1,8 +1,33 @@
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Person {
     private String name;
     private String surname;
     private LocalDate birthDate;
+    private Set<Person> children;
 
+    public Person(String name, String surname, LocalDate birthDate) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.children =  new HashSet<>();
+    }
+
+
+
+    public boolean adopt(Person child ){
+return this.children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthDate=" + birthDate +
+                ", children=" + children +
+                '}';
+    }
 }
