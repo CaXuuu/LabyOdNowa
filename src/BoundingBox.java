723639b99f82c0d7b30ebcd103e365e53b154e07
maciek <maciekmalyszek@gmail.com>
2025-08-29 10:45:53 +0200

    wyzerowanie

--- src/BoundingBox.java
+++
@@ -1,3 +0,0 @@
-public record BoundingBox(double x1, double y, double width, double height) {
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-Point p = new Point(2,3);
-        System.out.println(p.toString());
-        System.out.println(p.toSvg());
-SvgScene scene = new SvgScene();
-        Polygon p1 = new Polygon(new Point[]{
-                new Point(2.0, 6.5),
-                new Point(40.0, 50.5),
-                new Point(80.0, 99.5)
-        });
-        Polygon p2 = new Polygon(new Point[]{
-                new Point(32.0, 53.5),
-                new Point(32.0, 50.5),
-                new Point(39.0, 55)
-        });
-        Polygon p3 = new Polygon(new Point[]{
-                new Point(4.0, 8.5),
-                new Point(10, 20),
-                new Point(25, 45)
-        });
-        scene.addPolygon(p1);
-        scene.addPolygon(p2);
-        scene.addPolygon(p3);
-        System.out.println(scene);
-        System.out.println(scene.toSvg());
+
 
 
     }
