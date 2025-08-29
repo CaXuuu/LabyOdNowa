8f6e1da1a2da267ceadc2a3f36ea745d6f6e6bb0
maciek <maciekmalyszek@gmail.com>
2025-08-29 10:50:20 +0200

    Z4.1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+Person person = new Person();
+Person person2 = new Person();
+List<Person> persons = new ArrayList<Person>();
+persons.add(person);
+persons.add(person2);
+persons.stream()
+        .forEach(System.out::println);
+System.out.println(persons);
 
 
     }
---
+++ src/Person.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+
+}
