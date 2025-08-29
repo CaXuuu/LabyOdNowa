7b627712125729f11743a484bb146f6f2b364dc9
maciek <maciekmalyszek@gmail.com>
2025-08-29 08:42:53 +0200

    Z1.3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-
+Point p = new Point();
+p.x = 2;
+p.y=3;
+        System.out.println(p.toString());
+        System.out.println(p.toSvg());
 
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Point {
     public double x;
     public double y;
@@ -9,4 +11,21 @@ public class Point {
                 ", y=" + y +
                 '}';
     }
+
+    public String toSvg(){
+        return String.format(Locale.ENGLISH,"<circle r=\"5\" cx=\"%f\" cy=\"%f\" fill=\"red\" />", x, y);
+    }
+
+    public void translate(double dx, double dy){
+        x += dx;
+        y += dy;
+    }
+
+    public Point translated(double dx, double dy){
+        Point p = new Point();
+        p.x = this.x + dx;
+        p.y = this.y + dy;
+        return p;
+    }
+
 }
